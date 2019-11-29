325bc8409644b5fd8b0341fb61102a4baff69c79
hanl001 <hanling1217@gmail.com>
2019-11-29 22:56:10 +0800

    pass task scheduler

--- 621-TaskScheduler.java
+++ 621-TaskScheduler.java
@@ -1,5 +1,24 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        
+        if (tasks == null || tasks.length == 0) {
+            return 0;
+        }
+
+        int[] count = new int[26];
+        for (char c : tasks) {
+            ++count[c - 'A'];
+        }
+
+        Arrays.sort(count);
+        int i = 25;
+        while (i >= 0) {
+            if (count[i] == count[25]) {
+                --i;
+            } else {
+                break;
+            }
+        }
+
+        return Math.max(tasks.length, count[25] + n * (count[25] - 1) + 25 - i - 1);
     }
 }
