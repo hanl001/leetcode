7d83c3e03d504af33d0a75b2f6d3d5753a7a0db8
hanling <hanling1217@gmail.com>
2021-06-06 22:34:12 +0800

    daily commit

--- 664-StrangePrinter.java
+++ 664-StrangePrinter.java
@@ -1,11 +1,26 @@
 class Solution {
     public int strangePrinter(String s) {
-        int length = s.length;
+        int length = s.length();
         if (length <= 0) {
             return 0;
         }
-        int[][] dp = new int[length - 1][length - 1];
-        for (int i = 0; i < length; ++i) {
+        int[][] dp = new int[length][length];
+        for (int i = length - 1; i >= 0; --i) {
+            for (int j = i; j < length; ++j) {
+                if (i == j) {
+                    dp[i][i] = 1;
+                } else {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = dp[i][j - 1];
+                    } else {
+                        int min = Integer.MAX_VALUE;
+                        for (int k = i; k < j; ++k) {
+                            min = Math.min(min, dp[i][k] + dp[k + 1][j]);
+                        }
+                        dp[i][j] = min;
+                    }
+                }
+            }
         }
         return dp[0][length - 1];
     }
