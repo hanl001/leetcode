31dddc1f8d8aa19d13e1f979d54acb763c42dcdc
hanling <hanling1217@gmail.com>
2021-11-14 00:06:14 +0800

    array day 1

--- 53-MaximumSubarray.java
+++ 53-MaximumSubarray.java
@@ -1,5 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-
+        int sum = nums[0], start = 0, end = 0;
+        int result = sum;
+        for (int i = 1; i < nums.length; i++) {
+            if (sum < 0) {
+                sum = nums[i];
+            } else {
+                sum += nums[i];
+            }
+            result = Math.max(sum, result);
+        }
+        return result;
     }
 }
