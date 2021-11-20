9f75926cd2137658de84af498f06448d10b1646d
hanling <hanling1217@gmail.com>
2021-11-20 21:47:21 +0800

    daily code

--- source/0019-RemoveNthNodeFromEndofList.java
+++ source/0019-RemoveNthNodeFromEndofList.java
@@ -3,38 +3,54 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null) {
-            return null;
+        //ListNode fast = head;
+        //for (int i = 0; i < n; i++) {
+        //    fast = fast.next;
+        //}
+
+        //ListNode slow = null;
+        //while (fast != null) {
+        //    fast = fast.next;
+        //    // slow is nth pre node
+        //    if (slow == null) {
+        //        slow = head;
+        //    } else {
+        //        slow = slow.next;
+        //    }
+        //}
+
+        //// only head node is nth
+        //if (slow == null) {
+        //    return head.next;
+        //}
+
+        //ListNode nthNode = slow.next;
+        //slow.next = nthNode.next;
+        //return head;
+
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode first = head;
+        for (int i = 0; i < n; ++i) {
+            first = first.next;
         }
 
-        ListNode fast = head;
-        for (int i = 0; i < n; i++) {
-            fast = fast.next;
+        ListNode second = dummy;
+        second.next = head;
+        while (first != null) {
+            second = second.next;
+            first = first.next;
         }
-
-        ListNode slow = null; 
-        while (fast != null) {
-            fast = fast.next;
-            // slow is nth pre node
-            if (slow == null) {
-                slow = head;
-            } else {
-                slow = slow.next;
-            }
-        }
-    
-        // only head node is nth
-        if (slow == null) {
-            return head.next;
-        }
-
-        ListNode nthNode = slow.next;
-        slow.next = nthNode.next;
-        return head; 
+        
+        second.next = second.next.next;
+        return dummy.next;
     }
 }
+
