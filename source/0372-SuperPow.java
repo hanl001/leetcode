bf883352ec74162812ce46753283759840aea583
hanling <hanling1217@gmail.com>
2021-12-06 17:26:49 +0800

    daily code

--- source/0372-SuperPow.java
+++ source/0372-SuperPow.java
@@ -1,6 +1,8 @@
 class Solution {
     // Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+    // 你的任务是计算 ab 对 1337 取模，a 是一个正整数，b 是一个非常大的正整数且会以数组形式给出。
     public int superPow(int a, int[] b) {
         a = a % 1337;
+
     }
 }
