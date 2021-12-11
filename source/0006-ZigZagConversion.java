3110bb0479c16bc80c657b00470bc1af2a76ac95
hanling <hanling1217@gmail.com>
2021-12-11 21:25:19 +0800

    daily code

--- source/0006-ZigZagConversion.java
+++ source/0006-ZigZagConversion.java
@@ -6,9 +6,9 @@ class Solution {
         int step = 2 * numRows - 2;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j + i < s.length(); j += step) {
-                sb.append(s.charAt(j + i));
-                int index = i + j + 2 * (numRows - 1 - i);
+            for (int j = i; j < s.length(); j += step) {
+                sb.append(s.charAt(j));
+                int index = j + 2 * (numRows - 1 - i);
                 if (i > 0 && i < numRows - 1 && index < s.length()) {
                     sb.append(s.charAt(index));
                 }
