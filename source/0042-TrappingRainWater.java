19e27db275c5da66e7c2725b73fe258524d4c122
hanling <hanling1217@gmail.com>
2021-12-19 22:39:52 +0800

    daily code

--- source/0042-TrappingRainWater.java
+++ source/0042-TrappingRainWater.java
@@ -1,4 +1,55 @@
 class Solution {
+    
+    // 暴力法
+    public int trap(int[] height) {
+        int ret = 0;
+        // 第一个和最后一个不可能接到雨水
+        for (int i = 1; i < height.length - 1; i++) {
+            int curHeight = height[i];
+            int leftMax = 0;
+            for (int j = 0; j < i; j++) {
+                leftMax = Math.max(height[j], leftMax);
+            }
+            int rightMax = 0;
+            for (int j = i + 1; j < height.length; j++) {
+                rightMax = Math.max(height[j], rightMax);
+            }
+            int value = Math.min(leftMax, rightMax) - curHeight;
+            if (value > 0) {
+                ret += value;
+            }
+        }
+        return ret;
+    }
+    
+    // 前缀后缀 max 统计
+    public int trap(int[] height) {
+        if (height.length == 0) {
+            return 0;
+        }
+
+        int[] leftMax = new int[height.length];
+        leftMax[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        int[] rightMax = new int[height.length];
+        rightMax[height.length - 1] = height[height.length - 1];
+        for (int i = height.length - 2; i >= 0; i++) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int[] rightMax = new int[height.length];
+        int ret = 0;
+        // 第一个和最后一个不可能接到雨水
+        for (int i = 1; i < height.length - 1; i++) {
+            int curHeight = height[i];
+            int value = Math.min(leftMax[i], rightMax[i]) - curHeight;
+            ret += value;
+        }
+        return ret;
+    }
 
     // 递减栈
     public int trap(int[] height) {
@@ -8,7 +59,7 @@ class Solution {
         for (int i = 0; i < height.length; i++) {
             while (stackIndex > -1 && height[i] > height[stack[stackIndex]]) {
                 int bottom = stack[stackIndex--];
-                if (stackIndex == -1) {
+                if (stackIndex == -1) { // 边界无法操作
                     break;
                 }
                 int left = stack[stackIndex];
