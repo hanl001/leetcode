737843963e37d885d6e55e0c94940a3dfe463758
hanling <hanling1217@gmail.com>
2022-02-05 20:35:15 +0800

    daily code

--- source/0198-HouseRobber.java
+++ source/0198-HouseRobber.java
@@ -1,14 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
+        if (nums.length == 1) {
+            return nums[0];
         }
-        int dp0 = 0;
-        int dp1 = nums[0];
-        for (int i = 1; i < nums.length; ++i) {
-            int temp = dp1;
+        int dp0 = nums[0];
+        int dp1 = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            int oldDp1 = dp1;
             dp1 = Math.max(dp0 + nums[i], dp1);
-            dp0 = temp;
+            dp0 = oldDp1;
         }
         return dp1;
     }
